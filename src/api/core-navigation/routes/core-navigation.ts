ef7555dc95466f5e0e40ada9a7ad2871d5d66e10
Hiep Nguyen <hiepnh610@gmail.com>
2022-12-11 00:22:03 +0700

    feat: add some type

---
+++ src/api/core-navigation/routes/core-navigation.ts
@@ -0,0 +1,7 @@
+/**
+ * core-navigation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::core-navigation.core-navigation');
---
+++ src/api/core-navigation/services/core-navigation.ts
@@ -0,0 +1,7 @@
+/**
+ * core-navigation service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::core-navigation.core-navigation');
